0fdaaf9a05a835fe5775c4b71b417490c0514c1f
IlshatKhairullin <96662830+IlshatKhairullin@users.noreply.github.com>
2022-05-29 20:22:27 +0300

    task-product_search_button: интегрирована бд в поиск товаров
    Нахождение товара по таблице из бд реализована на регулярке из SQL

--- Parsers/United_parsers.py
+++ Parsers/United_parsers.py
@@ -1,19 +1,44 @@
-import re
+import psycopg2
+from Database.config import host, user, password, db_name, port
 from Parsers.Yandex_lavka_parser import Yandex_Lavka_food_data
 from Parsers.parser_perekrestok import Perekrestok_shop
 from Parsers.Metro_Parser import Metro
 
 
-def combine_parsers(product_name, address='Кубанская 54'):  # подумать про многопоточность, точно нужна при обработке запросов и скорее всего при парсинге 3-х сайтов
-    user_products = 'Яндекс Лавка\n\n'
+def combine_parsers(product_name, address='Кубанская 54'):  # у нас будет несколько баз данных и здесь при помощи
+    # библиотеки для работы с координатами. какой склад ближе по заданному адресу, по тому и работаем
 
-    lavka_dict = Yandex_Lavka_food_data(address)
-    # perekrestok_dict = Perekrestok_shop(address)
-    # metro_dict = Metro(address)
+    user_products_lavka = 'Яндекс Лавка\n\n'
+    user_products_metro = 'Метро\n\n'
+    user_products_perekrestok = 'Перекресток\n\n'
 
-    for i in lavka_dict.keys():
-        if product_name in i:
-            if re.match(product_name+str(' '), i):
-                user_products += f'{i}, цена - {str(lavka_dict[i][0])} рублей, вес - {lavka_dict[i][1]}\n'
-    # дальше будет еще 2 цикла для перекрестка и метро
-    return user_products
+    try:
+        # подключение к базе данных
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name,
+            port=port
+        )
+
+        with connection.cursor() as cursor:
+            sql = "SELECT product_name, price, amount FROM Yandex_Lavka_food_data WHERE product_name ~* %s"
+            search_product = "\m{}\M".format(product_name)
+            cursor.execute(sql, (search_product,))
+            prod_from_db = cursor.fetchall()
+
+    except Exception as _ex:
+        print("[INFO] Error while working with PostgreSQL", _ex)
+    finally:
+        if connection:
+            connection.close()
+            print("[INFO] PostgreSQL connection closed")
+
+    for i in prod_from_db:
+        user_products_lavka += f'{i[0]}, цена - {str(i[1])} рублей, вес - {i[2]}\n'
+    return user_products_lavka
+
+
+if __name__ == '__main__':
+    print(combine_parsers('^чипсы'))
