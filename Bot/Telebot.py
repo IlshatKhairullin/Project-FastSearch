5d7d0ad682ce95e2f90d87512a7f86ec6aec83f4
Fantacy-Fan <artem.harov.120@mail.ru>
2022-05-30 15:14:02 +0300

    Добавлена документацию в конпку "Помощь",изменени приветствие

--- Bot/Telebot.py
+++ Bot/Telebot.py
@@ -6,6 +6,7 @@ from Parsers.United_parsers import combine_parsers
 
 bot = telebot.TeleBot(Configure.config['token'])
 
+
 @bot.message_handler(commands=['help', 'start'])
 def start(message):
     global keyboard1
@@ -17,7 +18,10 @@ def start(message):
     bot_help = types.KeyboardButton(text='❓ Помощь')
     keyboard1.add(address, search_product_price, delivery, bot_help)
 
-    bot.send_message(message.chat.id, 'Привет, я телеграмм бот', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Приветствую, я телеграмм бот "FastSearch"\n\n'
+                                      'Прежде чем мы начнём я советую тебе перейти в раздел ❓ Помощь чтобы узнать как со мной работать \n\n'
+                                      'Ну что, приступим к работе?', reply_markup=keyboard1)
+
 
 @bot.message_handler(content_types=['text'])
 def callback_worker(message):
@@ -29,7 +33,8 @@ def callback_worker(message):
             keyboard.add(back)
             bot.send_message(message.chat.id, '🔎 Поиск товара')
 
-            product_name = bot.send_message(message.chat.id, 'Введите название продукта', reply_markup=keyboard)  # чтобы вернуться надо 2 раза нажать назад - пофиксить
+            product_name = bot.send_message(message.chat.id, 'Введите название продукта',
+                                            reply_markup=keyboard)  # чтобы вернуться надо 2 раза нажать назад - пофиксить
             bot.register_next_step_handler(product_name, product_info_func)
             flag = 1
             waitMethod()
@@ -59,9 +64,19 @@ def callback_worker(message):
             keyboard.add(back)
 
             bot.send_message(message.chat.id, '❓ Помощь', reply_markup=keyboard)
+            bot.send_message(message.chat.id,
+                             'В данном разделе вы сможите узнать как пользоваться телеграмм ботом "FastSearch"\n\n'
+                             'Каждая кнопка выполняет свой функционал\n\n'
+                             'Адрес доставки: Сохраняет введённый адрес для доставки со склада интернет магазинов\n\n'
+                             'Поиск товара: При вводе определенного продукта выведет информацию о нём\n\n'
+                             'Стоимость доставки: Рассчитывает стоимость доставки от склада интернет магазинов до введённого адреса\n\n'
+                             'Назад: Возвращает в главное меню\n\n'
+                             'Примечание*: Если вы ввели некоректный адрес доставки телеграмм бот выдаст ошибку,чтобы изменить адрес воспользуйтесь кнопкой "Адрес доставки" и введите корректный адрес',
+                             reply_markup=keyboard)
 
         if message.text == '⬅ Назад':
             bot.send_message(message.chat.id, '⬅ Назад', reply_markup=keyboard1)
+
 
 def waitMethod():
     while True:
@@ -70,9 +85,11 @@ def waitMethod():
         else:
             break
 
+
 def handle_address(user_address):
     global address
     address = user_address.text
+
 
 def product_info_func(product_name):
     global prod_info, flag
